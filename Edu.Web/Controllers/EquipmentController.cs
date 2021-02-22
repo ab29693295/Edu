6aaf6389d1d569f40ebc0dbacaa47b7a6465b344
志云 申 <296932795@qq.com>
2021-02-22 23:12:04 +0800

    设备

---
+++ Edu.Web/Controllers/EquipmentController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Edu.Entity;
+using Edu.Models;
+using Edu.Service;
+using Edu.Tools;
+
+namespace Edu.Web.Controllers
+{
+    public class EquipmentController : BaseControl
+    {
+        // GET: Equipment
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
