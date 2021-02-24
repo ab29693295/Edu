3512693bfc391648afbfa0fa19e745f16b32fd92
志云 申 <296932795@qq.com>
2021-02-24 23:16:27 +0800

    完善统计

---
+++ Edu.Web/Controllers/BorderController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Edu.Web.Controllers
+{
+    public class BorderController : Controller
+    {
+        // GET: Border
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
