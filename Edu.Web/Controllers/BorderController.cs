7ecd75d905086818b183309f6d8a917588b607b5
志云 申 <296932795@qq.com>
2021-02-24 23:16:47 +0800

    1

--- Edu.Web/Controllers/BorderController.cs
+++ Edu.Web/Controllers/BorderController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Edu.Service;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,7 +7,7 @@ using System.Web.Mvc;
 
 namespace Edu.Web.Controllers
 {
-    public class BorderController : Controller
+    public class BorderController : UserBaseController
     {
         // GET: Border
         public ActionResult Index()
