68be1b83b0d4b294341a8ac0048aaa085a944980
志云 申 <296932795@qq.com>
2021-03-02 23:27:46 +0800

    1

---
+++ Edu.Web/API/EquipMealController.cs
@@ -0,0 +1,42 @@
+﻿using Edu.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Edu.Web.API
+{
+    public class EquipMealController : BaseAPIController
+    {
+
+        [HttpGet]
+        /// <summary>
+        /// 获取美白信息
+        /// </summary>
+        /// <param name="EqCode"></param>
+        /// <returns></returns>
+        public IHttpActionResult GetEquipMeal(string EqCode)
+        {
+            try
+            {
+                var equip = unitOfWork.DEquipMeal.Get(p => p.EqCode == EqCode);
+
+                if (equip != null)
+                {
+                    return Json(new { R = true, Data = equip });
+                }
+                else
+                {
+                    return Json(new { R = false, m = "设备不存在！", Data = "" });
+                }
+
+            }
+            catch (Exception ex)
+            {
+                return Json(new { R = false, m = ex.ToString(), Data = "" });
+            }
+        }
+    }
+}
