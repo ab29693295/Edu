8b58b40bec49b9808d9c74a9d64e823996022b48
ab29693295 <296932795@qq.com>
2021-03-02 18:19:02 +0800

    付款搞定

---
+++ Edu.Web/Controllers/EquipMealController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Edu.Web.Controllers
+{
+    public class EquipMealController : Controller
+    {
+        // GET: EquipMeal
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
